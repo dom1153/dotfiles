8335ea2e9568948f92a4f04826b3ff5d506fcd54
Dominic Dang <dtdd1153@live.com>
2024-03-25 01:31:34 -0500

    correctly override nixpkg for zsh-autocomplete

--- dot_config/nix/home/terminal/shell/zsh.nix
+++ dot_config/nix/home/terminal/shell/zsh.nix
@@ -15,20 +15,6 @@
     # ".local/share/zsh/zsh-fast-syntax-highlighting".source = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh-fast-syntax-highlighting";
   };
 
-  nixpkgs.overlays = [
-    (final: prev: {
-      zsh-autocomplete = prev.zsh-autocomplete.overrideAttrs (oldAttrs: {
-        ### stolen from stable 23.11 packages ; workaround for https://github.com/marlonrichert/zsh-autocomplete/issues/645
-        src = prev.fetchFromGitHub {
-          owner = "marlonrichert";
-          repo = "zsh-autocomplete";
-          rev = "23.05.24";
-          sha256 = "sha256-/6V6IHwB5p0GT1u5SAiUa20LjFDSrMo731jFBq/bnpw=";
-        };
-      });
-    })
-  ];
-
   home.packages = with pkgs; [
     nix-zsh-completions
     zsh-autocomplete
--- dot_config/nix/system/nix/nixpkgs.nix
+++ dot_config/nix/system/nix/nixpkgs.nix
@@ -5,6 +5,22 @@
     ];
 
     overlays = [
+      (final: prev: {
+        zsh-autocomplete = prev.zsh-autocomplete.overrideAttrs (oldAttrs: {
+          ### grabbing stable 23.11 packages ; workaround for https://github.com/marlonrichert/zsh-autocomplete/issues/645
+          src = prev.fetchFromGitHub {
+            owner = "marlonrichert";
+            repo = "zsh-autocomplete";
+            rev = "23.05.24";
+            sha256 = "sha256-/6V6IHwB5p0GT1u5SAiUa20LjFDSrMo731jFBq/bnpw=";
+          };
+          installPhase = ''
+            install -D zsh-autocomplete.plugin.zsh $out/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+            cp -R scripts $out/share/zsh-autocomplete/scripts
+            cp -R functions $out/share/zsh-autocomplete/functions
+          '';
+        });
+      })
     ];
   };
 }
