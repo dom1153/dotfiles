0dc76a28b06ac1187c5e3587b2bf353c8b335f87
Dominic Dang <dtdd1153@live.com>
2024-03-26 14:12:09 -0500

    nssmdns -> dns4

--- dot_config/nix/system/services/avahi.nix
+++ dot_config/nix/system/services/avahi.nix
@@ -2,7 +2,8 @@
   ### extra info should hopefully allow mac to connect too
   services.avahi = {
     enable = true;
-    nssmdns = true;
+    # nssmdns = true;
+    nssmdns4 = true;
     openFirewall = true;
     publish = {
       enable = true;
