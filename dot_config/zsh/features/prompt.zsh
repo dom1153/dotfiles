e3418ecdba4ea892b370408905beaa01bcca19cb
Dominic Dang <dtdd1153@live.com>
2024-03-29 15:09:02 -0500

    fixing zsh to bash prompt flow in darwin

--- dot_config/zsh/features/prompt.zsh
+++ dot_config/zsh/features/prompt.zsh
@@ -1,10 +1,10 @@
-export PS1="╭─ %F{green}%n@%m%f | %F{blue}%W%f | %F{blue}%t %f
-│ %1~
-╰─%# "
-
-
 if type "starship" >/dev/null; then
   ### should remove warning for git branch taking too long to load
   export STARSHIP_LOG="error"
   eval "$(starship init zsh)"
+else
+  ### don't export by default ; keeps PS1 clean for zsh -> other shell flow
+  export PS1="╭─ %F{green}%n@%m%f | %F{blue}%W%f | %F{blue}%t %f
+  │ %1~
+  ╰─%# "
 fi
