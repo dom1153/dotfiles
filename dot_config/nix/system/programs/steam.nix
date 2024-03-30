89fec66df8368ffa14149699f9f118dbf8295b94
Dominic Dang <dtdd1153@live.com>
2024-03-29 21:48:17 -0500

    window rule stuff

--- dot_config/nix/system/programs/steam.nix
+++ dot_config/nix/system/programs/steam.nix
@@ -1,7 +1,15 @@
-{...}: {
+{pkgs, ...}: {
+  programs.java.enable = true;
+
   programs.steam = {
     enable = true;
+
+    package = pkgs.steam.override {withJava = true;};
+
     remotePlay.openFirewall = true;
+
     dedicatedServer.openFirewall = true;
+
+    gamescopeSession.enable = true;
   };
 }
