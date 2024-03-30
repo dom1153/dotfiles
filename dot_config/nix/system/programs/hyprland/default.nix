cfa1dfedad423cd8d6ef6d53a5b324d0adc0d21f
Dominic Dang <dtdd1153@live.com>
2024-03-30 03:30:41 -0500

    updating gdm comment

--- dot_config/nix/system/programs/hyprland/default.nix
+++ dot_config/nix/system/programs/hyprland/default.nix
@@ -2,7 +2,7 @@
   ### these dependancies are being configured for just hyprland, not in general
   ### otherwise I would move them to the services folder
   imports = [
-    # ./gdm.nix ### does not work well with plymouth boot
+    # ./gdm.nix ### does not work well with plymouth boot ; gdm starts after hyprland logs in???
     # ./gnome-services.nix
     ./greetd.nix
     # ./kwallet.nix
