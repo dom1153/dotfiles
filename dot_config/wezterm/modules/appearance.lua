9291ae4c5bab78b810db3f6aba9e9dc5b36df716
Dominic Dang <dtdd1153@live.com>
2024-04-08 13:54:25 -0500

    neverprompt and fix local domain font

--- dot_config/wezterm/modules/appearance.lua
+++ dot_config/wezterm/modules/appearance.lua
@@ -11,14 +11,19 @@ function module.apply_to_config(config)
 	-- fallback will report errors if not found
 	-- https://wezfurlong.org/wezterm/config/lua/wezterm/target_triple.html
 	-- Luckily, should include a nerd font out of the box
-	if hostname == "pixelbookgo-localdomain" then
+	if hostname == "stella-hoshii.local" then
 		config.font = wezterm.font("FiraCode Nerd Font Mono")
+		-- default is 12.0
+		config.font_size = 12.0
 	end
 
 	-- tab bar
 	config.use_fancy_tab_bar = false
 	config.hide_tab_bar_if_only_one_tab = true
 	config.tab_max_width = 999
+
+	-- behavior
+	config.window_close_confirmation = "NeverPrompt"
 end
 
 return module
