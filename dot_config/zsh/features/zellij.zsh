741071dfa4f2500f410e6a13f90beebaf16908ee
Dominic Dang <dtdd1153@live.com>
2024-04-07 21:05:54 -0500

    wsl things

--- dot_config/zsh/features/zellij.zsh
+++ dot_config/zsh/features/zellij.zsh
@@ -1,6 +1,21 @@
 ### default.kdl will be the default layout
 ### $TERM_PROGRAM == Apple_Terminal
 if [ -z $SSH_CONNECTION ] && [ -z $VSCODE_SHELL_INTEGRATION ]; then
-    eval "$(zellij setup --generate-auto-start zsh)"
+    if [[ -z "$ZELLIJ" ]]; then
+        if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
+            zellij attach -c
+        else
+            if type "wslvar" &> /dev/null
+                zellij --layout general
+            else
+                zellij
+            fi
+        fi
+
+        if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
+            exit
+        fi
+    fi
+
 fi
 
