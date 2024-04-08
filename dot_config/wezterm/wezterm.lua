30d9ef82299deff3ff5084727b1b0c165775d5c9
Dominic Dang <dtdd1153@live.com>
2024-04-08 13:46:31 -0500

    trying to wsl wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,8 +1,19 @@
 -- Pull in the wezterm API
+local wezterm = require("wezterm")
+
 local appearance = require("modules/appearance")
+local wsl = require("modules/wsl")
 
 -- This will hold the configuration.
 local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
 appearance.apply_to_config(config)
+wsl.apply_to_config(config)
 
 return config
