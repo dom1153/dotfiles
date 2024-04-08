446200462389969f9701b6aadd27db35c79a86dd
Dominic Dang <dtdd1153@live.com>
2024-04-08 14:14:18 -0500

    more wsl stuff for wez

--- dot_config/wezterm/modules/wsl.lua
+++ dot_config/wezterm/modules/wsl.lua
@@ -2,6 +2,8 @@ local wezterm = require("wezterm")
 local hostname = wezterm.hostname()
 
 local module = {}
+-- https://wezfurlong.org/wezterm/config/launch.html#__tabbed_1_2
+-- local launch_menu = {}
 
 function module.apply_to_config(config)
 	if hostname == "DomArtProW11" then
@@ -13,6 +15,9 @@ function module.apply_to_config(config)
 			},
 		}
 		config.default_domain = "WSL:NixOS"
+
+		-- right click menu to get options
+		config.hide_tab_bar_if_only_one_tab = false
 	end
 end
 
