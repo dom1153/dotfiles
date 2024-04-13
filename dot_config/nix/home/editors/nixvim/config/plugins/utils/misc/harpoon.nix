d36cb66ab3ad34b9c85ac3cc569e6d181f3e351e
Dominic Dang <dominicdang@protonmail.com>
2024-04-12 22:47:10 -0500

    harpoon group for nvim

--- dot_config/nix/home/editors/nixvim/config/plugins/utils/misc/harpoon.nix
+++ dot_config/nix/home/editors/nixvim/config/plugins/utils/misc/harpoon.nix
@@ -4,26 +4,7 @@
     enable = true;
     enableTelescope = true;
     keymapsSilent = true;
-    # keymaps = {
-    # addFile = "<leader>ha";
-    # toggleQuickMenu = "<leader>he";
-    # navFile = {
-    # "1" = "<leader>hj";
-    # "2" = "<leader>hk";
-    # "3" = "<leader>hl";
-    # "4" = "<leader>hm";
-    # };
-    # };
   };
 
   extraConfigLua = builtins.readFile ./lua/harpoon.lua;
-
-  keymaps = [
-    ### tab.nix
-    {
-      mode = "n";
-      key = "<leader>h";
-      action = "+harpoon";
-    }
-  ];
 }
--- dot_config/nix/home/editors/nixvim/config/plugins/utils/misc/lua/harpoon.lua
+++ dot_config/nix/home/editors/nixvim/config/plugins/utils/misc/lua/harpoon.lua
@@ -10,3 +10,13 @@ vim.keymap.set("n", "<leader>hj", function() ui.nav_file(2) end, { desc = "Go 2"
 vim.keymap.set("n", "<leader>hk", function() ui.nav_file(3) end, { desc = "Go 3" })
 vim.keymap.set("n", "<leader>hl", function() ui.nav_file(4) end, { desc = "Go 4" })
 --stylua: ignore end
+
+local wk = require("which-key")
+
+local spacing = "  "
+
+-- https://github.com/folke/which-key.nvim?tab=readme-ov-file#%EF%B8%8F-mappings
+wk.register({
+	-- basic config
+	h = { name = "󰈺" .. spacing .. "Harpoon" },
+}, { prefix = "<leader>" })
