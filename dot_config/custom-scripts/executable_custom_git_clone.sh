9911364ae1597beace08d0fc3cd27d16036b5090
Dominic Dang <dominicdang@protonmail.com>
2024-06-15 14:35:30 -0500

    steam autostart nixos and mroe custom gitclone

--- dot_config/custom-scripts/executable_custom_git_clone.sh
+++ dot_config/custom-scripts/executable_custom_git_clone.sh
@@ -5,11 +5,12 @@
 
 ### test cases: https://github.com/mipmip/home-manager-option-search
 ###             https://github.com/mipmip/home-manager-option-search/blob/main/scripts/parse_options-json.rb
+###             https://github.com/end-4/CirnOS
 ### general regex tester: https://www.regextester.com/109207
 
 # echo "arg1: $1"
-r=$(echo "$1" | grep -Eo 'https?://\w+\.\w+\w+/\w+/[^/]*')
-f=$(echo "$r" | sed -E 's/https?:\/\/\w+\.\w+\/(\w+)\/(\w+)/\2_\1/g')
+r=$(echo "$1" | grep -Eo 'https?://\w+\.\w+\w+/[^/]*/[^/]*')
+f=$(echo "$r" | sed -E 's/https?:\/\/\w+\.\w+\/(\S+)\/(\w+)/\2_\1/g')
 # echo "r: $r"
 # echo "f: $f"
 # echo git clone "'$r'" "'$f'"
--- dot_config/custom-scripts/executable_plasma_autostart.sh
+++ dot_config/custom-scripts/executable_plasma_autostart.sh
@@ -4,7 +4,8 @@ autorandr --load single &
 
 signal-desktop --start-in-tray &
 
-steam -silent &
+### it's fine since I use multiple workspaces
+# steam -silent &
 
 ### -f foreground (debug) ; ssl == crypto (no .conf option)
 # barriers --config $HOME/.config/custom-scripts/assets/jill_stella_dorothy.conf --disable-crypto &
