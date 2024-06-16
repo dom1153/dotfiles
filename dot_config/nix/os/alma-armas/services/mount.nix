9c85d41299beaf94f1808d259b4eee025dec39d2
Dominic Dang <dominicdang@protonmail.com>
2024-06-15 21:20:55 -0500

    trying mount fix server

--- dot_config/nix/os/alma-armas/services/mount.nix
+++ dot_config/nix/os/alma-armas/services/mount.nix
@@ -2,27 +2,33 @@
   ### lsblk -o name,mountpoint,label,size,uuid
   fileSystems."/run/media/archoo/lamp-700G" = {
     device = "/dev/disk/by-uuid/66C5574E674DAEDD";
-    fsType = "ntfs";
+    # fsType = "ntfs";
+    fsType = "ntfs-3g";
     options = [
       "nofail"
+      "rw"
       "users"
     ];
   };
 
   fileSystems."/run/media/archoo/super-4T" = {
     device = "/dev/disk/by-uuid/967AC4B27AC49103";
-    fsType = "ntfs";
+    # fsType = "ntfs";
+    fsType = "ntfs-3g";
     options = [
       "nofail"
-      "users"
+      "rw"
+      # "users"
     ];
   };
 
   fileSystems."/run/media/archoo/fuzzy-500G" = {
     device = "/dev/disk/by-uuid/22DC1805279A1C23";
-    fsType = "ntfs";
+    # fsType = "ntfs";
+    fsType = "ntfs-3g";
     options = [
       "nofail"
+      "rw"
       "users"
     ];
   };
