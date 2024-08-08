8a9038903e76056e85ba1a05703ce7d700111c83
Dominic Dang <dominicdang@protonmail.com>
2024-08-08 11:31:09 -0500

    playing with umask for server

--- dot_config/nix/os/alma-armas/services/mount.nix
+++ dot_config/nix/os/alma-armas/services/mount.nix
@@ -1,6 +1,9 @@
 {...}: {
   ### lsblk -o name,mountpoint,label,size,uuid
+  ### fuseblk / ntfs doesn't allow traditional mount options qq
   boot.supportedFilesystems = ["ntfs" "ntfs-3g"];
+  ### 'set' chmod 077 permissions for nextcloud... (umask flips in reverse)
+  # "umask=007"
   fileSystems."/run/media/archoo/fuzzy-500G" = {
     device = "/dev/disk/by-uuid/22DC1805279A1C23";
     # fsType = "ntfs";
@@ -27,6 +30,7 @@
     ];
   };
 
+  ### leaving this to root since I want to leave this to server-apps not personal files
   fileSystems."/run/media/archoo/super-4T" = {
     device = "/dev/disk/by-uuid/967AC4B27AC49103";
     # fsType = "ntfs";
