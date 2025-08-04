ffb88a800b32ac8f0239b1a983fe4052541a46e1
Dominic Dang <dominicdang@protonmail.com>
2025-08-04 10:14:48 -0500

    Disable wezterm close prompt

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,7 +4,9 @@ local config = wezterm.config_builder()
 
 config = {
   color_scheme = 'Catppuccin Mocha',
-  use_fancy_tab_bar = false
+  use_fancy_tab_bar = false,
+  window_close_confirmation = 'NeverPrompt',
+  hide_tab_bar_if_only_one_tab = true,
 }
 
 return config
