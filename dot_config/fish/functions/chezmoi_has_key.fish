db5c6c3ea872591bd7ce3ac2f6da478b1b77acbe
archoo <archoo@dev.flamingo-universe.ts.net>
2026-02-04 19:58:31 -1000

    Remove jq fish requirement

--- dot_config/fish/functions/chezmoi_has_key.fish
+++ dot_config/fish/functions/chezmoi_has_key.fish
@@ -1,8 +1,10 @@
 ### desc: wrapper for getting chezmoi data
 function chezmoi_has_key
-    if chezmoi data | jq -e "$argv[1]" > /dev/null
-        return 0 # True
-    else
-        return 1 # False
+    if type -q jq
+        if chezmoi data | jq -e "$argv[1]" > /dev/null
+            return 0 # True
+        else
+            return 1 # False
+        end
     end
 end
