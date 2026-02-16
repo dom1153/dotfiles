bff2cecd1467bd1f984ec347501a869b6dc5ba39
dom1153 <dominicdangjobs@gmail.com>
2026-02-16 13:34:54 -0600

    Dont output error messages

--- dot_config/fish/functions/chezmoi_has_key.fish
+++ dot_config/fish/functions/chezmoi_has_key.fish
@@ -1,7 +1,7 @@
 ### desc: wrapper for getting chezmoi data
 function chezmoi_has_key
     if type -q jq
-        and chezmoi data | jq -e "$argv[1]" >/dev/null
+        and chezmoi data | jq -e "$argv[1]" &>/dev/null
 
         return 0
     else
